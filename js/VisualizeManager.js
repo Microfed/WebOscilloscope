e0539940867a036b5e72d19a91436275efe0d7b8
Microfed <microfed@gmail.com>
2012-01-07 18:19:30 +0400

    - Добавлена тестовая имплементация визуализатора. - Реализованы классы визуализатора, менеджера.

--- js/VisualizeManager.js
+++ js/VisualizeManager.js
@@ -9,5 +9,24 @@
 var Visualize = {};
 
 Visualize.VisualizeManager = atom.Class({
+    initialize:function (address, sweep, countOfPoints) {
+        this.gridContext = this.getCanvasContext("grid");
+        this.dataContext = this.getCanvasContext("data");
+        this.visualizer = new Visualize.Visualizer(this.gridContext, this.dataContext, countOfPoints, sweep);
+        this.dataTransform = new Visualize.DataTransform(sweep, countOfPoints);
+        this.dataStream = new Visualize.DataStream(address);
+    },
+
+    getCanvasContext:function (name) {
+        var LC = LibCanvas.extract({}),
+            canvas = atom.dom('canvas').filter('#' + name).first,
+            context = canvas.getContext('2d-libcanvas');
+
+        return context;
+    },
+
+    drawCurrentFrame:function (data) {
+        self.visualizer.drawFrame(data);
+    }
 
 });
---
+++ js/main.js
@@ -0,0 +1,56 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: Microfed
+ * Date: 07.01.12
+ * Time: 15:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+Visualize.stopDrawing = false;
+Visualize.loop;
+
+atom.dom(function () {
+    var visualizeManager = new Visualize.VisualizeManager("localhost", 100, 800);
+});
+
+function CreateVisualizer(sweep, countOfPoints, maxAbsValue, countOfFrames) {
+    var visualizeManager = new Visualize.VisualizeManager("localhost", sweep, countOfPoints),
+        dataLength = countOfFrames,
+        periodLength = countOfPoints,
+        period = [],
+        data = [],
+        j = 0, i = 0, k = 0,
+        loop,
+        draw = function () {
+            var drawFrame = function (data) {
+                visualizeManager.visualizer.drawFrame(data);
+            };
+
+            visualizeManager.dataTransform.getFormattedData(data[k], drawFrame);
+            k++;
+            if (k === dataLength || Visualize.stopDrawing) {
+                clearInterval(loop);
+            }
+        }
+
+    for (j = 0; j < dataLength; j += 1) {
+        period = [];
+        for (i = 0; i < periodLength; i += 1) {
+            period[i] = Math.floor(Math.random() * maxAbsValue);
+        }
+        data.push(period);
+    }
+
+    Visualize.loop = setInterval(draw, 1);
+}
+
+function StartDrawing() {
+    var sweep = atom.dom('input').filter('#sweep').first.value,
+        countOfPoints = atom.dom('input').filter('#numberOfPoints').first.value,
+        maxAbsValue = atom.dom('input').filter('#maxValue').first.value,
+        countOfFrames = atom.dom('input').filter('#numberOfFrames').first.value;
+
+    if(sweep > 0, countOfPoints > 0, maxAbsValue > 0, countOfFrames > 1){
+        CreateVisualizer(sweep,countOfPoints,maxAbsValue,countOfFrames);
+    }
+}
